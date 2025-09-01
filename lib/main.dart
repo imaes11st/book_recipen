405b87107762e3e8435deb8ea0e48acf73fa8a57
imaes11st <imaes.st@gmail.com>
2025-08-31 21:07:45 -0500

    Organizando cards y configurando img

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:book_recipent/screens/home_screen.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -22,36 +21,20 @@ class RecipeBook extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.orange,
-        title: Text('Libro de Recetas', style: TextStyle(color: Colors.white)),
-      ),
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: 125,
-        child: Card(
-          child: Row(
-            children: <Widget>[
-              Container(
-                height: 125,
-                width: 100,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Container(),
-                ),
-              ),
-              SizedBox(width: 26),
-              Column(
-                children: <Widget>[
-                  Text('Lasagna'),
-                  Text('Juan M'),
-                  Container(height: 2, width: 75, color: Colors.orange),
-                ],
-              ),
-            ],
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.orange,
+          title: Text('Recipe Book', style: TextStyle(color: Colors.white)),
+          bottom: TabBar(
+            indicatorColor: Colors.white,
+            labelColor: Colors.white,
+            unselectedLabelColor: Colors.white,
+            tabs: [Tab(icon: Icon(Icons.home), text: 'Home')],
           ),
         ),
+        body: TabBarView(children: [HomeScreen()]),
       ),
     );
   }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[_RecipesCard(context), _RecipesCard(context)],
+      ),
+    );
+  }
+
+  Widget _RecipesCard(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 125,
+        child: Card(
+          child: Row(
+            children: <Widget>[
+              Container(
+                height: 125,
+                width: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: Colors.orange,
+                ),
+              ),
+              SizedBox(width: 26),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Lasagna',
+                    style: TextStyle(fontSize: 16, fontFamily: 'Quicksand'),
+                  ),
+                  SizedBox(height: 4),
+                  Container(height: 2, width: 75, color: Colors.orange),
+                  Text(
+                    'Alison J',
+                    style: TextStyle(fontSize: 16, fontFamily: 'Quicksand'),
+                  ),
+                  SizedBox(height: 4),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
