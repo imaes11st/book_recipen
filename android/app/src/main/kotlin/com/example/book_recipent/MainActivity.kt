4fdb505a20b3766a5025edc9974643f4599a7817
imaes11st <imaes.st@gmail.com>
2025-08-31 20:50:04 -0500

    Creando vistas y ajustando el main

---
+++ android/app/src/main/kotlin/com/example/book_recipent/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.book_recipent
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Hola Mundo',
+      home: RecipeBook(),
+    );
+  }
+}
+
+class RecipeBook extends StatelessWidget {
+  const RecipeBook({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: Text('Libro de Recetas', style: TextStyle(color: Colors.white)),
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 125,
+        child: Card(
+          child: Row(
+            children: <Widget>[
+              Container(
+                height: 125,
+                width: 100,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(),
+                ),
+              ),
+              SizedBox(width: 26),
+              Column(
+                children: <Widget>[
+                  Text('Lasagna'),
+                  Text('Juan M'),
+                  Container(height: 2, width: 75, color: Colors.orange),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
